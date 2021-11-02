e9770f8c6728d5b335185828e135c656482e82e7
Ysrilakshmivenkatarama <93367849+Ysrilakshmivenkatarama@users.noreply.github.com>
2021-11-02 13:57:27 +0530

    Add files via upload

---
+++ rtbextensioncprogram.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+   string raptor_prompt_variable_zzyz;
+   ?? b;
+   ?? result;
+   ?? a;
+
+   raptor_prompt_variable_zzyz ="Enter first number";
+   cout << raptor_prompt_variable_zzyz << endl;
+   cin >> a;
+   raptor_prompt_variable_zzyz ="Enter first number";
+   cout << raptor_prompt_variable_zzyz << endl;
+   cin >> b;
+   result =a+b;
+   cout << "result of the addition operation is"+result << endl;
+   return 0;
+}
