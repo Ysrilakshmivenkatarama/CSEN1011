8bed8c8d8de8c629a780b70526f7b247d963f3b4
Yenamandra sri lakshmi venkata rama sri <93367849+Ysrilakshmivenkatarama@users.noreply.github.com>
2021-11-26 06:06:52 +0000

    added a file

--- marksofsubject.c
+++ marksofsubject.c
@@ -1,2 +1,12 @@
 # include <stdio.h>
-int main()
+int main(){
+  int a,b,c,d,e,f;
+  float t,ave,pm ;
+  printf("enter marks of 5 subjects");
+  scanf("%d%d%d%d%d", &a,&b,&c,&d,&e);
+  t=a+b+c+d+e;
+  ave=(t)/5;
+  pm=(t/500)*100;
+  printf("the t is %f\n ave is %f\n pm is %f\n",t,ave,pm);
+  return 0;
+}
