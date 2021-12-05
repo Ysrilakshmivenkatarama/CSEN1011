a12afca796527662bbe0c1d3fb9c8d335ec42c1c
Ysrilakshmivenkatarama <93367849+Ysrilakshmivenkatarama@users.noreply.github.com>
2021-12-05 11:36:39 +0530

    Update and rename C programs to addition .c

---
+++ MODULE 1/addition .c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+	int main(){
+		int a,b,c;
+	
+		printf("enter any number");
+		scanf("%d", &a);
+		
+		printf("enter any number");
+		scanf("%d", &b);
+		c=a+b;
+		printf("your answer is %d", c);
+		return 0;
+}
