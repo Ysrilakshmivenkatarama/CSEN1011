af5a6c0c3c0714aaae5a07d71b3395c0ec031d34
Yenamandra sri lakshmi venkata rama sri <93367849+Ysrilakshmivenkatarama@users.noreply.github.com>
2021-12-07 09:17:40 +0000

    added a file

---
+++ armstrong.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+int n , n1, r , s = 0 ; 
+printf("enter a three digit number ");
+scanf("%d", &n);
+n1=n;
+while(n>0){
+  r = n%10 ;
+  s=s+(r*r*r);
+  n=n/10;
+}
+if(s==n)
+printf("%d is an armstrong number");
+else 
+printf("%d is not an armstrong number");
+
+return 0 ;
+}
