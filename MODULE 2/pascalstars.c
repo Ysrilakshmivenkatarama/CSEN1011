50449db8806f52babbbf170cfc812f166068b295
Ysrilakshmivenkatarama <93367849+Ysrilakshmivenkatarama@users.noreply.github.com>
2022-01-04 13:43:28 +0530

    Update pascalstars.c

--- MODULE 2/pascalstars.c
+++ MODULE 2/pascalstars.c
@@ -1,1 +1,17 @@
+#include<stdio.h>
+int main(){
+int  x, y, a;
+printf("the rows", a);
+scanf("%d", &a);
+for(x = 1; x<=a; ++x)
+{
+  for( y= 1 ; y <= x ; ++y)
+{
+  printf("*");
+}
+printf("\n");
+}
 
+return 0;
+
+}
